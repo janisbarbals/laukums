51fddc0ab07ba840ebf259503b2bdc257f979828
janisbarbals <janisbarbals520@gmail.com>
2022-04-19 15:12:47 +0300

    nepabeigts

--- script.js
+++ script.js
@@ -12,3 +12,42 @@ return false;
     
     }
 }
+function perimetrs(m1,m2,m3){
+    const m=nolasa();
+    m1=m.m1; m2=m.m2; m3=m.m3;
+    console.log(m1, m2, m3);
+    const p = m1 + m2 + m3;
+    console.log(p);
+    return(p);
+}
+function pusper(m1,m2,m3){
+    const pusper=perimetrs(m1, m2, m3)/2;
+    console.log(pusper);
+    return(pusper);
+}
+    function laukums(m1,m2,m3){
+        const pusper=perimetrs(m1, m2, m3)/2;
+        let tlaukums = Math.sqrt(pusper * ((pusper - m1) * (pusper - m2) *  (pusper - m3)));
+        console.log(m1, m2, m3);
+        const p = m1 + m2 + m3;
+        console.log(p);
+        return(p);
+}
+function rezultats(){
+    const m=nolasa();
+    m1=m.m1; m2=m.m2; m3=m.m3;
+    console.log(m1, m2, m3);
+    if (!nolasa()){
+        t="Nekorekti ievaddati, trijstura malu vertibā ir jabut lielakam par 0"
+    }
+
+}
+function izvadaTekstu(){
+    const teskst = rezultāts();
+    console.log(tesksts);
+    const sakne= document.getElimentById("izvade");
+    const sakne= document.getElimentById("p");
+    raksti.innerHTML = teksts;
+    sakne.appendChild(raksti);
+    
+}
